7513cfdfea53727271a0ea98d64e2128ba174dc5
ToF- <cthibaut@octo.com>
2018-03-11 22:20:58 +0100

    diamond 'B' make a 3 row diamond

--- C/test.c
+++ C/test.c
@@ -6,8 +6,18 @@ MU_TEST(test_single_row_diamond) {
     mu_assert_int_eq(1, max_output);
     mu_assert_string_eq("A", output[0]);
 }
+
+MU_TEST(test_three_row_diamond) {
+    diamond('B');
+    mu_assert_int_eq(3, max_output);
+    mu_assert_string_eq(" A ", output[0]);
+    mu_assert_string_eq("B B", output[1]);
+    mu_assert_string_eq(" A ", output[2]);
+}
+
 MU_TEST_SUITE(test_suite) {
 	MU_RUN_TEST(test_single_row_diamond);
+	MU_RUN_TEST(test_three_row_diamond);
 }
 int main(int argc, char *argv[]) {
 	MU_RUN_SUITE(test_suite);
