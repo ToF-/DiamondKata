ac26d98cb8419346df3866e8dec92053350bcad5
ToF <cthibaut@octo.com>
2018-03-11 10:14:02 +0100

    documenting sizes

--- C/diamond.h
+++ C/diamond.h
@@ -1,7 +1,9 @@
 #include <string.h>
-#define MAX_ROW (26)
+#define MAX_LETTER (26)
+#define MAX_ROW (MAX_LETTER*2-MAX_LETTER1)
+#define MAX_COL (MAX_LETTER*2)
 static int max_output = 0;
-static char output[MAX_ROW+1][MAX_ROW]; 
+static char output[MAX_COL][MAX_ROW]; 
 
 void diamond(char letter) {
     strcpy(output[max_output++], "A");
