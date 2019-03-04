12966631c08d0b73406c3d1fefe8302b7d9fac59
ToF <cthibaut@octo.com>
2019-03-04 18:26:59 +0100

    removed arrows (too sophisticated)

--- Haskell/Specs.hs
+++ Haskell/Specs.hs
@@ -2,31 +2,27 @@ import Test.Hspec
 import Test.QuickCheck
 import Diamond
 import Data.List (transpose)
-import Control.Arrow
 
 main = hspec $ do
     describe "a diamond" $ do
         let d = forAll $ fmap diamond $ choose ('A','Z')
-            equ f g = uncurry (==) . (f &&& g)
-            allTrue props = and . zipWith ($) props . repeat
 
         it "has its height equal to its width" $ 
-            d $ (length `equ` (length . transpose)) 
+            d $ \d -> length d == length (transpose d)
 
         it "is reversible vertically" $ 
-            d $ (id `equ` reverse)
+            d $ \d -> d == reverse d
 
         it "is reversible horizontally" $ 
-            d $ (id `equ` map reverse)
+            d $ \d -> d == map reverse d
 
         it "contains letters in order in each quarter" $ do
-            d $  let  size       = succ . (`div` 2) .  length 
-                      letters    = flip take ['A'..'Z'] . size 
-                      firstNonSp = head . dropWhile (==' ')
-                      nonSp      = map firstNonSp
-                      firstHalf  d = take (size d) d 
-                      secondHalf d = drop (pred (size d)) d 
-                      in allTrue [(nonSp . firstHalf) `equ` letters
-                                 ,(nonSp . secondHalf) `equ` (reverse . letters)
-                                 ,(nonSp . firstHalf . transpose) `equ` (reverse . letters)
-                                 ,(nonSp . secondHalf . transpose) `equ` letters]
+            d $ \d -> let  size d     = 1+length d `div` 2 
+                           letters d  = take (size d) ['A'..'Z'] 
+                           pattern      = map (head . dropWhile (==' '))
+                           firstHalf  d = take (size d) d 
+                           secondHalf d = drop (pred (size d)) d 
+                    in and [pattern (firstHalf d) == letters d
+                           ,pattern (secondHalf d) == reverse (letters d)
+                           ,pattern (firstHalf (transpose d)) == reverse (letters d)
+                           ,pattern (secondHalf (transpose d)) == letters d]
